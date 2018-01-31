bf56a5b8022402d1a9f2f2ce37692ccaa945a384
Arend-Jan Breukink <35037565+AJBreukink@users.noreply.github.com>
2018-01-31 14:48:35 +0100

    Add files via upload
    nieuwe pagina (addpost) om titel, omschrijving en content in te zenden

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@
     function fetchNews( $conn )
     {
 
-        $request = $conn->prepare(" SELECT news_id, news_title, news_desc, news_author FROM blogarticles ORDER BY news_id DESC ");
+        $request = $conn->prepare(" SELECT news_id, news_title, news_desc, news_author
+          FROM blogarticles
+          ORDER BY news_id DESC ");
         return $request->execute() ? $request->fetchAll() : false;
     }
 
@@ -12,14 +14,16 @@
     function getAnArticle( $id_article, $conn )
     {
 
-        $request =  $conn->prepare(" SELECT news_id,  news_title, news_content, news_author FROM blogarticles WHERE news_id = ? ");
+        $request =  $conn->prepare(" SELECT news_id,  news_title, news_content, news_author
+          FROM blogarticles WHERE news_id = ? ");
         return $request->execute(array($id_article)) ? $request->fetchAll() : false;
     }
 
 
     function getOtherArticles( $differ_id, $conn )
     {
-        $request =  $conn->prepare(" SELECT news_id,  news_title, news_desc, news_content, news_author FROM blogarticles  WHERE news_id != ? ");
+        $request =  $conn->prepare(" SELECT news_id,  news_title, news_desc, news_content, news_author
+          FROM blogarticles  WHERE news_id != ? ");
         return $request->execute(array($differ_id)) ? $request->fetchAll() : false;
     }
 
--- readnews.php
+++ readnews.php
@@ -1,68 +1,70 @@
 <?php require 'functions.php'; ?>
 <html>
 <head>
-    <title>AJ Blog</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+  <title>AJ Blog</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div class="container">
 
-        <div class="welcome">
-            <h1>AJ Blog</h1>
-            <p>Welkom op het test blog</p>
-            <a href="index.php">terug naar home page</a>
-        </div>
+  <div class="container">
 
-        <div class="news-box">
+    <div class="welcome">
+      <h1>AJ Blog</h1>
+      <p>Welkom op het test blog</p>
+      <a href="index.php">terug naar home page</a>
+      </div>
 
-            <div class="news">
-                <?php
-                    // get the database handler
-                    $dbh = connect_to_db();
-                    $id_article;
-                    if(isset($_GET['news_id'])){
-                    $id_article = (int)$_GET['news_id'];
-                  }
-                    $other_articles;
-                    if ( !empty($id_article) && $id_article > 0) {
-                        // Fetch news
-                        $article = getAnArticle( $id_article, $dbh );
-                        $article = $article[0];
-                        $other_articles = getOtherArticles( $id_article, $dbh );
+    <div class="news-box">
 
-                    }else{
-                        $article = false;
-                        echo "<strong>Verkeerd Artikel!</strong>";
-                    }
+        <div class="news">
+            <?php
+                // get the database handler
+                $dbh = connect_to_db();
+                $id_article;
+                if(isset($_GET['news_id'])){
+                $id_article = (int)$_GET['news_id'];
+              }
+                $other_articles;
+                if ( !empty($id_article) && $id_article > 0) {
+                    // Fetch news
+                    $article = getAnArticle( $id_article, $dbh );
+                    $article = $article[0];
+                    $other_articles = getOtherArticles( $id_article, $dbh );
+
+                }else{
+                    $article = false;
+                    echo "<strong>Verkeerd Artikel!</strong>";
+                }
 
 
                 ?>
 
-                <?php if (!empty($article) && $article) :?>
+            <?php if (!empty($article) && $article) :?>
 
-                <h2><?= stripslashes($article->news_title) ?></h2>
-                <!--<span>gepubliceerd op </?= date("M, jS  Y, H:i", $article->news_published_on) ?> by </?= stripslashes($article->news_author) ?></span>-->
-                <div>
-                    <?= stripslashes($article->news_content) ?>
-                </div>
+            <h2><?= stripslashes($article->news_title) ?></h2>
+            <!--<span>gepubliceerd op </?= date("M, jS  Y, H:i", $article->news_published_on) ?> by </?= stripslashes($article->news_author) ?></span>-->
+            <div>
+                <?= stripslashes($article->news_content) ?>
+            </div>
             <?php else:?>
 
-                <?php endif?>
-            </div>
+            <?php endif?>
+        </div>
 
-            <hr>
-            <h1>andere artikelen</h1>
-            <div class="similar-posts">
-                <?php if ( !empty($other_articles) && $other_articles ) :?>
+        <hr>
+        <h3>andere artikelen</h3>
 
-                <?php foreach ($other_articles as $key => $article) :?>
-                <h2><a href="readnews.php?news_id=<?= $article->news_id ?>"><?= stripslashes($article->news_title) ?></a></h2>
-                <p><?= stripslashes($article->news_desc) ?></p>
-                <?php endforeach?>
+        <div class="similar-posts">
+          <?php if ( !empty($other_articles) && $other_articles ) :?>
 
-                <?php endif?>
+          <?php foreach ($other_articles as $key => $article) :?>
+          <h2><a href="readnews.php?news_id=<?= $article->news_id ?>"><?= stripslashes($article->news_title) ?></a></h2>
+          <p><?= stripslashes($article->news_desc) ?></p>
+          <?php endforeach?>
 
-            </div>
+          <?php endif?>
+          </div>
 
         </div>
 
